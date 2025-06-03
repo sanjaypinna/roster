5e49a9c63c6ed04a929fd5a544870681834bfc9f
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-03 23:23:20 +0530

    Refactor Employer interface and enhance Project interface in profile types

--- types/profile.ts
+++ types/profile.ts
@@ -1,17 +1,33 @@
 // types/profile.ts
+
+export interface Project {
+  video?: string;
+  title: string;
+  duration: string;
+  type: string;
+  contribution: string;
+}
+
 export interface Employer {
-    name: string;
-    videos: string[];
-    title: string;
-    duration: string;
-    type: string;
-    contribution: string;
-  }
-  
+  name: string;
+  projects: Project[];
+}
+
   export interface Profile {
     name: string;
     summary: string;
     profileImage: string;
     employers: Employer[];
+    tags: string[];
   }
   
+
+  export interface Job {
+    id: string;
+    title: string;
+    company: string;
+    tags: string[];
+    description: string;
+    matchScore?: number;
+    matchedTags?: string[];
+  }
