414f27a0614d0094c63093be9f6a2cc29cf65b00
sanjaypinna <sanjaypinna265@gmail.com>
2025-06-03 19:13:59 +0530

    Add Layout component and integrate Navbar into the application

---
+++ pages/Layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import Navbar from "./component/Navbar";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main className="container mx-auto px-4">{children}</main>
+    </>
+  );
+}
